c70e0216044960e42c2d8b53adcfb6ef8a2dad3a
Askie Lin <askie_lin@trendmicro.com>
2021-10-26 22:12:47 +0800

    feat: add visual icon to identify status and error

--- status-and-alert/login/index.js
+++ status-and-alert/login/index.js
@@ -4,15 +4,21 @@ $(document).ready(function (e) {
     $('#status').html('');
 
     if ($('#account').val() === '') {
-      $('#alert').append('<p class="alert alert-danger">請輸入帳號</p>');
+      $('#alert').append(
+        '<p class="alert alert-danger"><i class="fas fa-exclamation"></i> 請輸入帳號</p>'
+      );
     }
 
     if ($('#password').val() === '') {
-      $('#alert').append('<p class="alert alert-danger">請輸入密碼</p>');
+      $('#alert').append(
+        '<p class="alert alert-danger"><i class="fas fa-exclamation"></i> 請輸入密碼</p>'
+      );
     }
 
     if ($('#account').val() !== '' && $('#password').val() !== '') {
-      $('#status').append('<p class="alert alert-success">已登入成功</p>');
+      $('#status').append(
+        '<p class="alert alert-success"><i class="fas fa-check"></i> 已登入成功</p>'
+      );
     }
 
     return false;
